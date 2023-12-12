3a64d2f2d48de99f44b61266057ccfada235b1a0
youlajang <136407043+youlajang@users.noreply.github.com>
2023-12-12 13:57:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ app = Flask(__name__)
 def hello():
 	return "Hello World!"
 
-@app.route('/hello')
-def hello():
-	return "Hello yula World!"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80)
